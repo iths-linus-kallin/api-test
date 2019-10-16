d39fad51845efe3d8d282b54d3510ba66f6819e7
Linus Kallin <54602452+iths-linus-kallin@users.noreply.github.com>
2019-10-16 13:08:21 +0200

    Funktionalitet men bl.a sticky funkar ej

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+let data;
+
 async function getData(){
     let response = await fetch('https://www.amiiboapi.com/api/amiibo/')
     let responseBody = await response.json()
@@ -6,27 +8,24 @@ async function getData(){
 
 async function render(){
     let list = await getData()
+    data = list
     let ul = document.querySelector("ul")
     let itemPrototype = document.querySelector("li.prototype")
     for(let item of list){
         let newItem = itemPrototype.cloneNode(true)
         newItem.classList.remove("prototype")
-        let p = newItem.querySelector("p")
-        p.innerText = item.name
-        ul.append(newItem)
+        newItem.classList.add("characters")
+        newItem.addEventListener("click", function(){
+            let photo = document.querySelector(".photo")
+            let name = document.querySelector(".name")
+            let gameSeries = document.querySelector(".gameSeries")
+            photo.src = item.image
+            name.innerHTML = item.name
+            gameSeries.innerHTML = item.gameSeries
+        })
+    newItem.innerText = item.name
+    ul.append(newItem)
     }
 }
 
-function showData(){
-    let photo = document.querySelector(".photo")
-    let name = document.querySelector(".name")
-    let gameSeries = document.querySelector(".gameSeries")
-    document.querySelector("li.prototype").addEventListener("click", function(){
-        photo.src = item.image
-        name.innerHTML = item.name
-        gameSeries.innerHTML = item.gameSeries
-    })
-}
-
 render()
-showData()
